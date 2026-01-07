a6dfa19e815bdc31837792a34293e624cd82fd2b
Izzat888 <izzatjorayev907@gmail.com>
2026-01-08 00:29:29 +0500

    header qismi

---
+++ assets/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Header.css"
+import { FaTelegram, FaInstagram, } from "react-icons/fa";
+import one from './images/zamon.svg'
+function Header() {
+  return (
+    <header className='header'>
+        <div className='container'>
+            <div className='header__container'>
+                <a className='logo' href="#"><img className='header__logo' src={one} alt="Logo"/></a>
+                <ul className='header__list'>
+                    <li className='header__item'>
+                        <a className='header-list__link' href="#">Home</a>
+                    </li>
+                    <li className='header__item'>
+                        <a className='header-list__link' href="#">About</a>
+                    </li>
+                    <li className='header__item'>
+                        <a className='header-list__link' href="#">Tours</a>
+                    </li>
+                    <li className='header__item'>
+                        <a className='header-list__link' href="#">Contacts</a>
+                    </li>
+                </ul>
+                <div className='icon'>
+                    <a className='header-icon__link' href="#"><FaTelegram size={24}/></a>
+                    <a className='header-icon__link' href="#"><FaInstagram size={24}/></a>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./main.css"
+import "./App.css"
+import Header from '../assets/components/Header/Header'
+function App() {
+  return (
+    <>
+    <div>
+      <Header/>
+    </div>
+    </>
+  )
+}
+
+export default App
